b5bd1a84d94a0be77b5fe666bbbf5592d968367e
geza steinert <gwasteinert@gmail.com>
2022-07-27 17:31:35 +0200

    change all pictures

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Floating Head of Geza welcomes you to his Homepage!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.schauspielervideos.de/fullprofile/schauspieler-geza-willem-artus-steinert.html"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          I am an Actor!
         </a>
       </header>
     </div>
