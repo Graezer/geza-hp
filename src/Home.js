f717e286ae48998c071766ac28a17f0e66e25291
geza steinert <gwasteinert@gmail.com>
2022-08-10 18:20:18 +0200

    make burger menu work, beautify homepage

--- src/Home.js
+++ src/Home.js
@@ -7,33 +7,41 @@ function Home() {
   return (
     <div className='form-container'>
       <div className='banner'>
-        <h1>Geza Steinert</h1>
-        <p>web developer and actor</p>
-        <img className="App-logo" src={logo} alt="logo" />
-          <p>choose a path you want to continue on..</p>
+        <div className='is-flex is-align-items-center'>
+          <img className="App-logo" src={logo} alt="logo" />
+          <div>
+            <h1>Geza Steinert</h1>
+            <h3>Web Developer and Actor</h3>
+          </div>
+        </div>
+        <br/>
+        <br/>
+        <br/>
       </div>
 
       <div className='content'>
         <div className='coding'>
-
-          <Link className="m-2 App-link" to="/coding">see coding projects and external links</Link> 
+          <Link className="m-2 App-link" to="/coding">see coding projects etc.</Link> 
         </div>
 
         <div>
-          <p className='m-5'>AND</p>
+          <p className='m-5'>OR</p>
         </div>
-        
-        <div className='acting is-flex is-align-content-center'>
 
-          <Link className="m-2 App-link" to="/acting">see external links to my acting career</Link> 
-          <a
-            className="App-link"
-            href="https://www.schauspielervideos.de/fullprofile/schauspieler-geza-willem-artus-steinert.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Portfolio
-          </a>
+        <div className='acting is-flex is-align-items-center'>
+          <Link className="m-2 App-link" to="/acting">
+            have a look at my acting career
+          </Link> 
+            <div>
+              <p className='m-5'>OR</p>
+            </div>
+          <div className=''>
+            <a className="App-link m-3"
+              href="https://www.schauspielervideos.de/fullprofile/schauspieler-geza-willem-artus-steinert.html"
+              rel="noopener noreferrer">
+              my Portfolio
+            </a>
+          </div>
         </div>
       </div>
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,35 +4,37 @@ import logo from '../logo.svg'
 import './navbar.scss'
 
 function NavBar() {
-  
+
+  const [isActive, setisActive] = React.useState(false);
+
   return (
     <div>
       <nav className="navbar" role="navigation" aria-label="main navigation">
-        
+        <Link className="" to="/"><img src={logo} alt="logo" /></Link>
         <div className="navbar-brand">
-          <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+          <a
+            onClick={() => {
+              setisActive(!isActive);
+            }}
+            role="button"
+            href='#'
+            className={`navbar-burger burger ${isActive ? "is-active" : ""}`}
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbarBasicExample"
+          >
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
           </a>
         </div>
-
-        <div id="navbarBasicExample" className="navbar-menu">
+        <div id="navbarBasicExample" className={`navbar-menu ${isActive ? "is-active" : ""}`}>
           <div className="navbar-start">
-            <Link className="navbar" to="/"><img src={logo} alt="logo" /></Link>
-            
-            <div className="navbar-item has-dropdown is-hoverable">
-              <a className="navbar-link">
-                More
-              </a>
-
-              <div className="navbar-dropdown">
-                <Link className="navbar-item" to="/coding">Coding</Link> 
-                <Link className="navbar-item" to="/actor">Actor</Link> 
-                <Link className="navbar-item" to="/art">Art</Link>
-                <Link className="navbar-item" to="/contact">Contact</Link>
-              </div>
-            </div>
+            <Link className="navbar-item" to="/coding">Coding</Link> 
+            <Link className="navbar-item" to="/actor">Actor</Link> 
+            <Link className="navbar-item" to="/art">Art</Link>
+            <Link className="navbar-item" to="/contact">Contact</Link>
+            <Link className="navbar-item" to="/">Home</Link> 
           </div>
         </div>
       </nav>
