5708fe380f5058282e448ad0470a804bd71b48b5
geza steinert <gwasteinert@gmail.com>
2022-08-12 15:20:22 +0200

    get billion second nearly to work

--- src/projects/BillionBirthday.js
+++ src/projects/BillionBirthday.js
@@ -4,34 +4,50 @@ import moment from 'moment'
 function BillionBirthday() {
 
   const [year, setYear] = useState('')
-  const handleSubmit = e => {
+  const [month, setMonth] = useState('')
+  const [day, setDay] = useState('')
+  const [hour, setHour] = useState('12')
+  const [minute, setMinute] = useState('1')
+  const date = {year, month, day, hour, minute}
+  
+  const handleSubmit = (e) => {
     e.preventDefault(); // macht, dass die Seite nicht neu geladen wird
-    const year = setYear(e.target.value)
-    setYear('')
-    console.log(year)
+    console.log(date)
+    setYear("")
+    setMonth("")
+    setDay("")
+    setHour("")
+    setMinute("")
   };
-
-  function Calculate (y,m,d,h,min){
-    return (moment(`'${y}-${m}-${d} ${h}:${min}:00'`).add(10**9, 'seconds').format('LLL')
+  
+  function calculate(y,m,d,h,min){
+    return (moment(`${y}-${m}-${d} ${h}:${min}:00`).add(10**9, 'seconds').format('LLL')
     )
   }
 
-  const calc = (x) => {Calculate(1990,12,14,12,1)}
-  console.log(calc)
+  const calcDate = (x) => {calculate(date.year,date.month,date.day,date.hour,date.minute)}
+  console.log(calcDate())
 
   return (
     <div className='content'>
       <label>Calculate your Billionth Second Birthday </label>
       <form className='' onSubmit={handleSubmit}>
-        <input type="text" className='input is-info' placeholder='Year YYYY' pattern='[0-9]*' value="year"/>
-        <input type="text" className='input is-info' placeholder='Month MM' pattern='[0-9]*' value="month" />
-        <input type="text" className='input is-info' placeholder='Day DD' pattern='[0-9]*' value="day" />
-        <input type="text" className='input is-info' placeholder='Hour HH' pattern='[0-9]*' value="hour"/>
-        <input type="text" className='input is-info' placeholder='Minute HH' pattern='[0-9]*' value="minute"/>
-        <button type="submit" onSubmit={handleSubmit} className='button is-dark'>Go</button>
-        <h2>Your Billion Second Birthday</h2>
+        <p>Year: {year}</p>
+        <p>Month: {month}</p>
+        <p>day: {day}</p>
+        <p>hour: {hour}</p>
+        <p>minute: {minute}</p>
+
+        <input type="text" onChange={(e) => setYear(e.target.value)} className='input is-info' placeholder='Year YYYY' pattern='[0-9]*' value={year}  />
+        <input type="text" onChange={(e) => setMonth(e.target.value)} className='input is-info' placeholder='Month MM' pattern='[0-9]*' value={month} />
+        <input type="text" onChange={(e) => setDay(e.target.value)} className='input is-info' placeholder='Day DD' pattern='[0-9]*' value={day} />
+        <input type="text" onChange={(e) => setHour(e.target.value)} className='input is-info' placeholder='Hour HH' pattern='[0-9]*' value={hour}/>
+        <input type="text" onChange={(e) => setMinute(e.target.value)} className='input is-info' placeholder='Minute HH' pattern='[0-9]*' value={minute}/>
+        
+        <button type="submit" className='button is-dark'>Go</button> 
+        <h2>Your Billion Second Birthday is: {calcDate}</h2>
       </form>
-      <p>I used <a className='App-link m-3' href="https://momentjs.com/" target="_blank" rel="noreferrer">Moment.js</a> to calculate the date</p>
+      <p>Date calculated with<a className='App-link m-3' href="https://momentjs.com/" target="_blank" rel="noreferrer">Moment.js</a></p>
     </div>
   )
 }
